6a6916ef49bb79c3eab2cc321cabbb590c1fca49
misu <misu@aptpod.co.jp>
2017-08-07 19:40:55 +0900

    modify

--- tour-of-go/exercise-images.go
+++ tour-of-go/exercise-images.go
@@ -25,4 +25,8 @@ func (i *Image) At(x, y int) color.Color {
 func main() {
 	m := new(Image)
 	pic.ShowImage(m)
+
+	// 上記のようにポインタのレシーバでは
+	// m := Image{} とやるとmはポインタ型ではないのでレシーバとして認識されない
+	// m := new(Type) か m := &Image{}でポインタを受け取るようにする
 }
