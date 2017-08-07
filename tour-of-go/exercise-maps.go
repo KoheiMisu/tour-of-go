ce17b079b138be1565569274a738c340535c0add
misu <misu@aptpod.co.jp>
2017-08-07 13:02:09 +0900

    exercise-map

--- tour-of-go/exercise-maps.go
+++ tour-of-go/exercise-maps.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	strings := strings.Fields(s)
+	wordCountSet := make(map[string]int, len(strings))
+
+	for _, str := range strings {
+		if _, ok := wordCountSet[str]; ok {
+			wordCountSet[str]++
+		} else {
+			wordCountSet[str] = 1
+		}
+	}
+
+	return wordCountSet
+}
+
+func main() {
+	wc.Test(WordCount)
+}
